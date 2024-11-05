dcd18b5e171aefe2fce58f4d051d205ce4994617
ncguilbeault <ncguilbeault@gmail.com>
2024-11-05 18:28:22 +0000

    Updated data iterator to handle outputting position bins

--- examples/Decoder/PositionDecodingFromHippocampalSortedSpikes/decoder/data_iterator.py
+++ examples/Decoder/PositionDecodingFromHippocampalSortedSpikes/decoder/data_iterator.py
@@ -6,28 +6,20 @@ class DataIterator:
                  data: dict,
                  start_index: int = 0):
         self.data = data
-        self.position_bins = self.data["position_bins"]
+        self.keys = list(self.data.keys())
         self.index = start_index
         super().__init__()
     
-    def next(self,
-             loop: bool = True) -> tuple[list, list]:
+    def next(self) -> tuple[list, list]:
             
-        output = None
-        position_data = self.data["position_data"]
-        spike_times = self.data["spike_times"]
-        decoding_results = self.data["decoding_results"]
+        output = []
 
-        if self.index > len(position_data) and loop:
-            self.index = 0
+        for key in self.keys:
+            try:
+                output.append(self.data[key][self.index])
+            except IndexError:
+                output.append(self.data[key][0])
 
-        if self.index < len(position_data):
-            position = position_data[self.index]
-            spikes = spike_times[self.index]
-            decoding = decoding_results[self.index][np.newaxis]
+        self.index += 1
 
-            self.index += 1
-
-            output = (position, spikes, decoding, self.position_bins)
-        
-        return output
+        return (output, self.keys)
