ebd1f6cb1dd67746f002523be4a70d438a7d249a
ncguilbeault <ncguilbeault@gmail.com>
2024-11-06 12:54:06 +0000

    Updated init file

--- examples/Decoder/PositionDecodingFromHippocampalSortedSpikes/decoder/__init__.py
+++ examples/Decoder/PositionDecodingFromHippocampalSortedSpikes/decoder/__init__.py
@@ -1,4 +1,5 @@
-from .core import SortedSpikeDecoder, ClusterlessSpikeDecoder
+from .decoder import SortedSpikeDecoder, ClusterlessSpikeDecoder
 from .data_loader import DataLoader
 from .data_iterator import DataIterator
 from .model_loader import ModelLoader
+from .likelihood import LIKELIHOOD_FUNCTION
