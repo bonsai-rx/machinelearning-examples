1043f167881f798090fa61e650132451da90fce8
ncguilbeault <ncguilbeault@gmail.com>
2024-11-18 18:46:21 +0000

    Removed python package from inside example folder

--- examples/NeuralDecoding/PositionDecodingFromHippocampus/decoder/__init__.py
+++
@@ -1,4 +0,0 @@
-from .decoder import SortedSpikeDecoder, ClusterlessSpikeDecoder
-from .data_loader import DataLoader
-from .data_iterator import DataIterator
-from .likelihood import LIKELIHOOD_FUNCTION
